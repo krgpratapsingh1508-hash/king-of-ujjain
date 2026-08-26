1d9e490a64fad2e113850e4dc8c416cd35c609d0
krgpratapsingh1508-hash <krgpratapsingh1508@gmail.com>
2026-08-26 21:10:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,15 +26,14 @@ pd_st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-# Line 29 ki jagah ye code likhein:
-col1, col2 = pd_st.columns([0.15, 0.85])  # Dono ka ratio (size) set karne ke liye
+col1, col2 = pd_st.columns([0.15, 0.85])
 
 with col1:
-    # "logo.png" ki jagah GitHub par jo aapki file ka sahi naam hai wo likhein
-    # width badhakar (jaise 80, 100, 120) aap image ka size bada ya chhota kar sakte hain
-    pd_st.image("Logo.png", width=100) 
+    # Image ka alignment poore text ke beech me karne ke liye padding badha sakte hain
+    pd_st.image("Logo.png", width=110) 
 
 with col2:
+    # Dono text ab image ke right side me ek ke niche ek aayenge
     pd_st.title("ॐ गुरुवे नमः")
     pd_st.title("College CCE Foil Sheet Generator")
 pd_st.subheader("Institute of Law, Govt. Kamlaraja Girls Post-Graduate Autonomous College, Gwalior,(M.P.)")
